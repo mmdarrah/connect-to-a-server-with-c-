6d0d118b3ba6a236db095d61a7f6b6d054c4f87c
Hamade <mmdarrah@hotmail.com>
2020-09-22 13:04:09 +0200

    send list of books need some work

--- Avdelningsrapport/Form1.Designer.cs
+++ Avdelningsrapport/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(568, 329);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 108);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect to the server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Send all objects";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Load List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(233, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Send selected object";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(589, 489);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Avdelningsrapport/Form1.cs
+++ Avdelningsrapport/Form1.cs
@@ -1,20 +1,247 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Avdelningsrapport
 {
     public partial class Form1 : Form
     {
+        // Create the variables
+        private int port = 12345;
+        private string IpAddress = "127.0.0.1";
+
+        // Create the TcpClient object
+        private TcpClient client = new TcpClient();
+
         public Form1()
         {
             InitializeComponent();
+            client.NoDelay = true;
+        }
+
+        //List box the will contain all the data (List of books)
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //A button to start connection
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress serverIP = IPAddress.Parse(IpAddress);
+            Starconnection(serverIP);
+        }
+
+        // A button to send all the list
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (client.Connected)
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    listBox1.SetSelected(i, true);
+                byte[] message = Encoding.Unicode.GetBytes(listBox1.Text.ToString());
+                    StartTransmissionList(message);
+                }
+
+            }
+        }
+
+        // A button to load the list of books 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fileLoader();
+        }
+
+        // A button to send the selected book with the use of ToString 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (books.Count > 0)
+            {
+                if (listBox1.SelectedItems.Count == 1)
+                {
+                    StartTransmission(listBox1.SelectedItem.ToString());
+                    books.RemoveAt(listBox1.SelectedIndex);
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                }
+            }
+        }
+
+
+        // Async metod for the connection
+        private async void Starconnection(IPAddress serverIP)
+        {
+            try
+            {
+                await client.ConnectAsync(serverIP, port);
+                button1.Enabled = false;
+                listBox1.Enabled = true;
+            }
+            catch (Exception error)// in case of error
+            {
+                MessageBox.Show(error.Message, this.Text);
+                return;
+            }
+        }
+
+        // A async method to send all the list
+        private async void StartTransmissionList(byte[] message)
+        {
+            try
+            {
+                await client.GetStream().WriteAsync(message, 0, message.Length);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, this.Text);
+                return;
+            }
+        }
+
+        // A async method to send selected book
+        public async void StartTransmission(string message)
+
+        {
+            byte[] utData = Encoding.Unicode.GetBytes(listBox1.Text);
+            try
+            {
+                await client.GetStream().WriteAsync(utData, 0, utData.Length);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, Text);
+                return;
+            }
+
+        }
+
+        // Create a list of Books
+        List<Book> books = new List<Book>();
+        public void fileLoader()
+        {
+            // Check if th text file exist
+            if (File.Exists("texter.txt"))
+            {
+                // Creating a list of strings
+                List<string> items = new List<string>();
+                // StreamReader to load a text, line by line
+                StreamReader reader = new StreamReader("texter.txt", Encoding.Default, false);
+                string item = "";
+                //Continue to read until you reach end of file
+                while ((item = reader.ReadLine()) != null)
+                {
+                    items.Add(item);
+                }
+                //Lopping 
+                foreach (string a in items)
+                {
+                    string[] vektor = a.Split(new string[] { "###" }, StringSplitOptions.None);
+                    //Put each part in the correct variable
+                    string titel = vektor[0];
+                    string författare = vektor[1];
+                    string type = vektor[2];// the type will be uesd in the switch to determine the book class
+                    bool available = Convert.ToBoolean(vektor[3]);
+
+
+                    switch (type)
+                    {
+                        case "Roman":
+                            Novel novelBook = new Novel(titel, författare, available);
+                            books.Add(novelBook);
+                            break;
+
+                        case "Novellsamling":
+                            Journal journalBbook = new Journal(titel, författare, available);
+                            books.Add(journalBbook);
+                            break;
+
+                        case "Tidskrift":
+                            ShorStory shorStoryBook = new ShorStory(titel, författare, available);
+                            books.Add(shorStoryBook);
+                            break;
+                    }
+
+                }
+
+            }
+            else
+            {
+                //A message will be show if the text file is missing
+                MessageBox.Show("File is not found");
+            }
+
+
+            // Loop through books to add each one in the listBox
+            for (int i = 0; i < books.Count; i++)
+                {
+                    listBox1.Items.Add(books.ElementAt(i));
+                }
+        }
+
+       
+    }
+
+    // Declaring the base class
+    class Book
+    {
+        // Class properties all are public strings
+        public string bookTitle;
+        public string bookAuthor;
+        public string bookType;
+        public bool tillgänglig; // A new bool variable because it is in the text file. Maybe we need it in the future
+
+        // Class constructor
+        public Book(string title, string author, bool available)
+        {
+            bookTitle = title;
+            bookAuthor = author;
+            tillgänglig = available;
+        }
+    }
+
+    // Subclass with one more propertie
+    class Novel : Book
+    {
+        //Inherited constructor
+        public Novel(string title, string author, bool available) : base(title, author, available)
+        {
+            bookType = "(Novel)";
+        }
+
+        public override string ToString()
+        {
+            return bookTitle + " of " + bookAuthor + ". " + bookType;
+        }
+    }
+    // Subclass with one more propertie
+    class Journal : Book
+    {
+        //Inherited constructor
+        public Journal(string title, string author, bool available) : base(title, author, available)
+        {
+            bookType = "(Journal)";
+        }
+        public override string ToString()
+        {
+            return bookTitle + " of " + bookAuthor + ". " + bookType;
+        }
+    }
+    // Subclass with one more propertie
+    class ShorStory : Book
+    {
+        //Inherited constructor
+        public ShorStory(string title, string author, bool available) : base(title, author, available)
+        {
+            bookType = "(Short story)";
+        }
+        public override string ToString()
+        {
+            return bookTitle + " of " + bookAuthor + ". " + bookType;
         }
     }
 }
